37ee4494469db9039ea1faadc05385c9c22c8fc5
Phillip Hughes <phillipwins8@gmail.com>
2024-08-29 00:54:29 -0400

    submission

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from 'react';
+import Form from './Form';
+import TodoList from './TodoList';
+
+let id = 0;
+let getId = () => ++id;
+
+const initialTodos = [
+  { id: getId(), name: "Walk the dog", completed: false },
+  { id: getId(), name: "Learn React", completed: true },
+  { id: getId(), name: "Have fun", completed: false },
+];
 
 export default class App extends React.Component {
+  state = {
+    todos: initialTodos,
+    showAll: true
+  };
+
+  addTodo = (name) => {
+    this.setState({
+      todos: this.state.todos.concat({ id: getId(), completed: false, name })
+    });
+  };
+
+  toggleCompletion = (id) => {
+    this.setState({
+      todos: this.state.todos.map(td => {
+        if (id === td.id) return { ...td, completed: !td.completed };
+        return td;
+      })
+    });
+  };
+
+  toggleShowAll = evt => {
+    this.setState({
+        ...this.state,
+        
+        showAll: !this.state.showAll,
+    })
+  }
+
   render() {
     return (
       <div>
-        Todo App
+        <TodoList todos={this.state.todos} toggleCompletion={this.toggleCompletion} showAll={this.state.showAll} />
+        <Form addTodo={this.addTodo} />
+        <button onClick={this.toggleShowAll}>{this.state.showAll ? 'Hide Completed Todo': 'Show All'}</button>
       </div>
-    )
+    );
   }
 }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Todo from './Todo';
 
 export default class TodoList extends React.Component {
-  render() {
-    return (
-      <div>
-        TodoList
+  
+    
+
+    render() {
+      const { todos, toggleCompletion } = this.props
+      const filtered = todos.filter(td => !td.completed || this.props.showAll)
+      return (
+        <div>
+          {
+            filtered.map((todo) => (<Todo key={todo.id} todo={todo} toggleCompletion={toggleCompletion} />))
+          }
+        
       </div>
-    )
+    );
   }
 }
